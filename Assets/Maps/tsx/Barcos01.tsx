48f33ca09c649f858ec231de051c28e7b40256b7
Nicole Alvarez <ngalvare@hotmai.com>
2019-09-16 13:40:12 -0500

    Se agrego el cuerto

--- Assets/Maps/tsx/Barcos01.tsx
+++ Assets/Maps/tsx/Barcos01.tsx
@@ -1,4 +1,2 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Barcos01" tilewidth="32" tileheight="32" tilecount="296" columns="8">
- <image source="../sprites/Barcos01.png" width="256" height="1184"/>
-</tileset>
+<tileset version="1.2" tiledversion="1.2.4" name="Mueble01" tilewidth="32" tileheight="32" tilecount="0" columns="0"/>
