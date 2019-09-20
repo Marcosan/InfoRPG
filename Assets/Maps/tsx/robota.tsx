82319e53a8b2add0c339dc56ea8fd5b3aed6326a
Nicole Alvarez <ngalvare@hotmai.com>
2019-09-20 17:09:04 -0500

    Se agrego lobby completo

---
+++ Assets/Maps/tsx/robota.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="robota" tilewidth="32" tileheight="31" tilecount="256" columns="16">
+ <image source="../sprites/robota01.png" width="512" height="512"/>
+</tileset>
---
+++ Assets/Maps/tsx/robots.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="robots" tilewidth="32" tileheight="31" tilecount="42" columns="7">
+ <image source="../sprites/robots.png" width="240" height="192"/>
+</tileset>
