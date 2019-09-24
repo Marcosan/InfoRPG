b47744ab268a468f399beecad91f0f88cb4adb28
Nicole Alvarez <ngalvare@hotmai.com>
2019-09-24 00:22:21 -0500

    Compelto About US

---
+++ Assets/Maps/tsx/comida.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="comida" tilewidth="32" tileheight="32" tilecount="300" columns="15">
+ <image source="../sprites/comida.png" width="500" height="652"/>
+</tileset>
---
+++ Assets/Maps/tsx/comida2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="comida2" tilewidth="32" tileheight="32" tilecount="56" columns="8">
+ <image source="../sprites/comida2.gif" width="269" height="244"/>
+</tileset>
